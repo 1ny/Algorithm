efb2b40671a8cb46a51db2f4dd7efc6fd6426265
1ny <78059973+1ny@users.noreply.github.com>
2025-04-05 21:44:16 +0900

    [Gold I] Title: 2048 (Easy), Time: 64 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+#define MAX 20
+
+int n;
+int arr[MAX][MAX];
+int arr2[MAX][MAX];
+
+void init(){
+    n = 0;
+    memset(arr, 0, sizeof(arr));
+    memset(arr2, 0, sizeof(arr2));
+}
+
+void rotate() {
+    int tmp[MAX][MAX];
+    memcpy(tmp, arr2, sizeof(tmp));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            arr2[i][j] = tmp[n - 1 - j][i];
+        }
+    }
+}
+
+void rotate_l() {
+    int tmp[MAX][MAX];
+    memcpy(tmp, arr2, sizeof(tmp));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            arr2[i][j] = tmp[j][n - i - 1];
+        }
+    }
+}
+
+/*
+func() 위로 밀기
+1. 위에서부터 연속 같은 수 2개씩 합
+2. 다 위로 올리기
+*/
+
+void func() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if ((arr2[j][i] != 0) && (arr2[j][i] != arr2[k][i]) && (arr2[k][i] != 0)) break;
+                if ((arr2[j][i] != 0) && (arr2[j][i] == arr2[k][i])) {
+                    arr2[j][i] *= 2;
+                    arr2[k][i] = 0;
+                    j = k;
+                }
+            }
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 1; i < n-k; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr2[i][j] && arr2[i - 1][j] == 0) {
+                    arr2[i - 1][j] = arr2[i][j];
+                    arr2[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+        scanf("%d", &n);
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                scanf("%d", &arr[i][j]);
+
+        int max = 0;
+        for (int k = 0; k < 1024; k++) {
+            memcpy(arr2, arr, sizeof(arr));
+            int temp = k;   // rotate 횟수
+            for (int i = 0; i < 5; i++) {
+                int dir = temp % 4; // 0~4
+                temp /= 4;
+
+                for (int j = 0; j < dir; j++) {
+                    rotate();
+                }
+                func();
+                for (int j = 0; j < dir; j++) {
+                    rotate_l();
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (arr2[i][j] > max) max = arr2[i][j];
+                }
+            }
+        }
+
+        printf("%d", max);
+    return 0;
+}
